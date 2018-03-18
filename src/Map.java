da833d1e95faa31a0f11df4af32d340257ab57fd
Martin Borbola <borbolam14@gmail.com>
2018-03-18 18:25:45 +0100

    Coordiantes example

--- src/Map.java
+++ src/Map.java
@@ -5,25 +5,34 @@ import java.util.List;
 public class Map {
     private List<Worker> workers;
     private Field[][] fields;
-    //private List<Field> fields;
 
     public void moveWorker(int id, Direction direction){
         System.out.printf("moveWorker");
-            workers.get(id).move(direction);
+        workers.get(id).move(direction);
     }
 
-    Map(int mapHeight, int mapWidth){
+    /*    x0 x1       x2        x3 x4        x5        x6 x7
+       y0                                fields[5][0]
+       y1
+       y2
+       y3
+       y4
+       y5         fields[2][5]
+       y6
+       y7
+        */
+    Map(int mapWidth, int mapHeight){
         workers=new ArrayList<>();
-        fields = new Field[mapHeight][mapWidth];
+        fields = new Field[mapWidth][mapHeight];
     }
 
     public void addWorker(Worker worker){
         System.out.printf("addWorker");
-            workers.add(worker);
+        workers.add(worker);
     }
 
-    public void addField(int xPos, int yPos){
-        System.out.println("addField");
-        //TODO: figure out which is the x and y axis in the fields[][]
+    public void addField(int xPos, int yPos, Field field){
+        System.out.println("Adding Field ");
+
     }
 }
